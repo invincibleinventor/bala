b8e948db0d2602c37ee835de75eb80b4475fa7e5
Invincible Inventor <invincibleinventor@gmail.com>
2023-11-13 17:20:52 +0530

    ok

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,29 +1,53 @@
-'use client'
-import type { Metadata } from 'next';
-import { CodeBlock, atomOneDark, vs2015 } from 'react-code-blocks';
 
+"use client"
+import Link from "next/link"
 
+export default function Contact() {
 
-export default function Home() {
- 
-      const showLineNumbers = true;
-      const wrapLines = true;
-      const codeBlock = true;
-      return (
-        <div
-        className='h-full font-mono'
-        >
-          <CodeBlock
-customStyle={{
-  height:'100%',
-}}         text={`//contact page work in progress`}
+    async function handleSubmit(event) {
 
-            language={'jsx'}
-            theme={vs2015}
-            {...{ showLineNumbers, wrapLines, codeBlock }}
-          />
-        </div>
-      );
-      
+        event.preventDefault();
+        const formData = new FormData(event.target)
+        try {
   
+            const response = await fetch('/api/contact', {
+                method: 'post',
+                body: formData,
+            });
+
+            if (!response.ok) {
+                console.log("falling over")
+                throw new Error(`response status: ${response.status}`);
+            }
+            const responseData = await response.json();
+            console.log(responseData['message'])
+    
+            alert('Message successfully sent');
+        } catch (err) {
+            console.error(err);
+            alert("Error, please try resubmitting the form");
+        }
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center" >
+       
+
+            <form onSubmit={handleSubmit} className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+                <div className="mb-4 flex flex-col w-500">
+
+                    <label htmlFor="form-name">Name </label>
+                    <input id="form-name" autoComplete="name" maxLength={50}  name="name" className="text-black"/>
+
+                    <label htmlFor="form-email"> Email:</label>
+                    <input id="form-email" required autoComplete="email" maxLength={80} name="email" type="email" className="text-black"/>
+
+                    <label htmlFor="form-message"> Message: </label>
+                    <textarea id="form-message" required name="message" rows={5} className="text-black" />
+
+                </div>
+                <button className=" rounded bg-sky-400" type="submit">Send</button>
+            </form>
+        </main>
+    )
 }
