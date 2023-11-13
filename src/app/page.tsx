b1d887f4cca3b0cfd82e5280ee057934e9ccba56
Invincible Inventor <invincibleinventor@gmail.com>
2023-11-13 15:24:05 +0530

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       return (
         
         
-          <div className='overflow-h-scroll flex h-auto flex-col content-center  items-center'>
+          <div className='flex h-auto flex-col content-center items-center  overflow-y-scroll'>
             <div className="my-auto mb-4 flex w-full flex-col-reverse md:mb-0 md:flex-row">
 <div className="flex flex-col  content-center px-8 py-6 md:w-3/5 md:px-14 md:py-10">
          <h1 className="font-neutral text-3xl text-neutral-300">
