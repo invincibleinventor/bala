0f0fbed4bb507208e8d347f4f505c3bd9f2dd3d4
Invincible Inventor <invincibleinventor@gmail.com>
2023-11-14 22:00:10 +0530

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                       <div className="no-scrollbar overflow-x-scroll">
                       <Filebar></Filebar>
                       </div>
-                      <div className=' h-[calc(100vh-88px)] w-auto overflow-x-hidden pb-10 md:pb-0'>
+                      <div className=' h-[calc(100vh-88px)] w-auto overflow-x-hidden pb-12 md:pb-0'>
           {children}
           </div>
           </div>
