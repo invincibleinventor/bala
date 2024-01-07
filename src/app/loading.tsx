83846ae0fee2cbf4f983b782725628a65c5d3dde
Invincible Inventor <invincibleinventor@gmail.com>
2024-01-07 16:44:59 +0530

    ok

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <div className="no-scrollbar flex h-auto overflow-x-hidden  w-[calc(100vw-58px)] flex-col content-center items-center overflow-y-scroll md:py-0 lg:overflow-hidden">
-      <div className="flex flex-col items-center content-center">
-        <div className="mx-auto text-neutral-400 text-xs">loading awesomeness...</div>
+      <div className="flex flex-col h-full items-center content-center">
+        <div className="my-10 text-neutral-400 text-xs">loading awesomeness...</div>
       </div>
     </div>
   );
